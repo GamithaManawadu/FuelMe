8cc48ae693b7b41b5acaab36ed6c0328f8f545ae
Gamitha <gamithamanawadu26@gmail.com>
2022-10-26 20:33:16 +0530

    updt

--- FuelMe - Frontend/app/src/main/java/com/example/FuelMe/WelcomeActivity.java
+++ FuelMe - Frontend/app/src/main/java/com/example/FuelMe/WelcomeActivity.java
@@ -4,11 +4,51 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 public class WelcomeActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 5000;
+
+    ImageView image;
+    TextView logo, slogan;
+    Animation topAnim, bottomAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        image = findViewById(R.id.imageView);
+        logo = findViewById(R.id.textView);
+        slogan = findViewById(R.id.textView2);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+        },SPLASH_SCREEN);
+
     }
 }
+
+//Set animation to elements
